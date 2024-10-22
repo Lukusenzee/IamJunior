46b54ccf72edd9219656c9b842833a768703282e
Lukusenzee <merkeliunasl@gmail.com>
2024-10-22 18:15:08 +0300

    commit

--- Baigiamasis/baigiamoji_uzduotis/src/app/page.js
+++ Baigiamasis/baigiamoji_uzduotis/src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <h1>Final Web</h1>
       <p>This website's purpose is to display gain knowledge during study period
       Further deepen knowledge of Javascript/HTML, by periodicaly adding new funtionality to this web.
-      Hopefully will be able to develop as advanced app/webs as with using java.</p>
+      Hopefully will be able to develop as advanced app/webs as with using java.This website is created using React.js and its layout.S</p>
       </div>
       <Carousell/>
       <div>
